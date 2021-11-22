ebf17140803b6ea45df2a21ac6db92ce4498614a
MikeCamel <mike@p2ptrust.org>
2021-11-22 13:55:55 +0000

    Added SHA256 hashing
    Signed-off-by: MikeCamel <mike@p2ptrust.org>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use chrono::{Utc, DateTime};
 use std::time::{SystemTime};
 use std::io::{stdin, stdout, Result, Write};
+use sha2::{Sha256, Digest};
 
 fn main() -> Result<()> {
     let mut buffer = String::new();
     stdin().read_line(&mut buffer)?;
     
     let stripped = buffer.trim();
+    let mut hasher = Sha256::new();
+    hasher.update(stripped);
+    let hashresult = hasher.finalize();
     
     let now = SystemTime::now();
     let now_chrono: DateTime<Utc> = DateTime::from(now);
-    let stamp = format!("{} - [{:?}]\n", stripped,now_chrono.to_rfc2822());
+
+    println!("Input = {}", stripped);
+    let stamp = format!("{:02x} - [{:?}]\n", hashresult ,now_chrono.to_rfc2822());
 
     stdout().write_all(stamp.as_bytes())?;
 
